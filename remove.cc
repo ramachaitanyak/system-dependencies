5263d3c876f34f52ebca0ba62d499971f799349b
Ramachaitanya Kavuluru <rkavuluru@ebay.com>
2021-12-11 16:15:54 -0800

    [Nit fix]
    Signed-off-by: Ramachaitanya Kavuluru <rkavuluru@ebay.com>

--- remove.cc
+++
@@ -1,9 +0,0 @@
-#include "remove.h"
-
-void
-Remove::remove_sw(std::string sw, std::map<std::string, std::vector<std::string>> used_by_map,
-        std::vector<std::string>) {
-    // Remove sw if it is not in used_by_map and currently installed
-    //
-
-}
